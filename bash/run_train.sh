fd112fa08421a1add74fb8e4f3706fa3766fadba
pavelihno <crazzyyellow02@gmail.com>
2026-04-26 10:55:18 +0200

    run_train bash script

---
+++ bash/run_train.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#BSUB -J master_thesis_train
+#BSUB -q gpua100
+#BSUB -W 4:00
+#BSUB -n 4
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -R "span[hosts=1]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -o logs/train_%J.out
+#BSUB -e logs/train_%J.err
+#BSUB -B
+#BSUB -N
+
+# -- Load Required Modules -- 
+module load cuda/11.6
+module load python3/3.10.18
+
+# -- Activate Environment --
+source /zhome/5b/6/228538/projects/master-thesis/my_env/bin/activate
+
+# -- Set Environment Variables --
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+export PM4PY_SHOW_PROGRESS_BAR=False
+export PM4PY_SHOW_INTERNAL_WARNINGS=False
+
+# -- Run Script --
+python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/process_transformer/BPIC_20_DD.yaml --device=cuda
--- bash/test.sh
+++ bash/test.sh
@@ -9,13 +9,14 @@
 #BSUB -B
 #BSUB -N
 
+# -- Activate Environment --
 source /zhome/5b/6/228538/projects/master-thesis/my_env/bin/activate
 
-# Set your variables
+# -- Set Environment Variables --
 export PYTHONPATH=$PYTHONPATH:$(pwd)
 
 export PM4PY_SHOW_PROGRESS_BAR=False
 export PM4PY_SHOW_INTERNAL_WARNINGS=False
 
-# Run your code
-python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/darwin/BPIC_13_C.yaml --device=cpu
+# -- Run Script --
+python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/darwin/BPIC_13_C.yaml --device=auto
