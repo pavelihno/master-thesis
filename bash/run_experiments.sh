8dc123874c7403255a05639a5c2402d244d3b21f
pavelihno <crazzyyellow02@gmail.com>
2026-04-26 16:21:14 +0200

    bash scripts for run_experiments and run_hyperparam_search

--- bash/run_experiments.sh
+++ bash/run_experiments.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/usr/bin/env bash
 set -euo pipefail
 
-#BSUB -J master_thesis_train
+#BSUB -J master_thesis_experiments
 #BSUB -q gpua100
 #BSUB -W 12:00
 #BSUB -n 4
 #BSUB -R "rusage[mem=1GB]"
 #BSUB -R "span[hosts=1]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -o logs/train_%J.out
-#BSUB -e logs/train_%J.err
+#BSUB -o logs/experiments_%J.out
+#BSUB -e logs/experiments_%J.err
 #BSUB -N
 
-# -- Load Required Modules -- 
+# -- Load Required Modules --
 module load cuda/11.6
 module load python3/3.10.18
 
@@ -20,10 +20,9 @@ module load python3/3.10.18
 source /zhome/5b/6/228538/projects/master-thesis/my_env/bin/activate
 
 # -- Set Environment Variables --
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
+export PYTHONPATH="$PYTHONPATH:$(pwd)"
 export PM4PY_SHOW_PROGRESS_BAR=False
 export PM4PY_SHOW_INTERNAL_WARNINGS=False
 
 # -- Run Script --
-python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/process_transformer/BPIC_20_DD.yaml --device=cuda
+python -u experiments/streaming/run_experiments.py --base-path=conf/experiments/streaming/next_activity/baseline/process_transformer --report-dir=experiments/outputs/streaming/next_activity/baseline/process_transformer --device=cuda --workers=4
--- bash/test.sh
+++ bash/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 #BSUB -J test
 #BSUB -q hpc
