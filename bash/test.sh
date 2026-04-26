bef9a7a03adae8fc11b9a9d18f16133bfb26546d
pavelihno <crazzyyellow02@gmail.com>
2026-04-26 18:36:56 +0200

    test.sh

--- bash/test.sh
+++ bash/test.sh
@@ -14,10 +14,9 @@ set -euo pipefail
 source /zhome/5b/6/228538/projects/master-thesis/my_env/bin/activate
 
 # -- Set Environment Variables --
-export PYTHONPATH=$PYTHONPATH:$(pwd)
-
+export PYTHONPATH=$(pwd)
 export PM4PY_SHOW_PROGRESS_BAR=False
 export PM4PY_SHOW_INTERNAL_WARNINGS=False
 
 # -- Run Script --
-python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/darwin/BPIC_13_C.yaml --device=auto
+python -u experiments/streaming/run_train.py "$CONFIG_PATH" --device=auto
