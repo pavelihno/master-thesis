93968169fe453f05bea9742332406521e4124465
pavelihno <crazzyyellow02@gmail.com>
2026-04-26 10:31:26 +0200

    test bash script

---
+++ bash/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#BSUB -J test
+#BSUB -q hpc
+#BSUB -W 15
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -o logs/test_%J.out
+#BSUB -e logs/test_%J.err
+#BSUB -B
+#BSUB -N
+
+source /zhome/5b/6/228538/projects/master-thesis/my_env/bin/activate
+
+# Set your variables
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+export PM4PY_SHOW_PROGRESS_BAR=False
+export PM4PY_SHOW_INTERNAL_WARNINGS=False
+
+# Run your code
+python -u experiments/streaming/run_train.py conf/experiments/streaming/next_activity/baseline/darwin/BPIC_13_C.yaml --device=cpu
